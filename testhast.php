0b6c9b8e10f30e524f0f794b73a1e95dad4bdcc2
ziazayn <karinasulistyadksd@gmail.com>
2025-11-17 10:24:19 +0700

    First commit

--- testhast.php
+++
@@ -1,9 +0,0 @@
-// File: test_hash.php
-<?php
-// Ini adalah password yang harus Anda gunakan untuk login: admin123
-$raw_password = 'admin123'; 
-$new_hash = password_hash($raw_password, PASSWORD_BCRYPT);
-
-echo "HASH BARU yang harus disalin: <br>";
-echo "<b>" . $new_hash . "</b>";
-?>
